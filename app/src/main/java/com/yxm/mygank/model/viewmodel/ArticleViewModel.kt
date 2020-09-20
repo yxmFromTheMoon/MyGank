3cea27a907de42d770601f14322705cfc3c579d6
xiangming.yin <xiangming.yin@maibaapp.cn>
2020-09-20 11:42:53 +0800

    feat:增加kotlin支持

---
+++ app/src/main/java/com/yxm/mygank/model/viewmodel/ArticleViewModel.kt
@@ -0,0 +1,12 @@
+package com.yxm.mygank.model.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author yxm
+ * @email yxmbest@163.com
+ * 2020/9/20 8:43
+ * @description
+ */
+class ArticleViewModel:ViewModel() {
+}
